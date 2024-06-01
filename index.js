0a33dc801afe90b6940d9a7a0f76bc165979d38c
shohsultoncode <shohsulton.3039@gmail.com>
2024-06-01 14:55:18 +0500

    add: last fucking commit

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const axios = require("axios");
 const connectDB = require("./config/index");
 const User = require("./config/models/user.model");
 const UserVideo = require("./config/models/video.model");
+const Table = require("cli-table");
 
-const TOKEN = process.env.TOKEN;  
+const TOKEN = process.env.TOKEN;
 const bot = new TelegramApi(TOKEN, { polling: true });
 
 // Connect to the database
@@ -14,84 +15,144 @@ connectDB();
 // Function to download Instagram video
 async function downloadInstagram(insta_url) {
   try {
-    const response = await axios.get("https://instagram-media-downloader.p.rapidapi.com/rapid/post.php", {
-      params: { url: insta_url },
-      headers: {
-        "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
-        "X-RapidAPI-Host": "instagram-media-downloader.p.rapidapi.com",
-      },
-    });
+    const response = await axios.get(
+      "https://instagram-media-downloader.p.rapidapi.com/rapid/post.php",
+      {
+        params: { url: insta_url },
+        headers: {
+          "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
+          "X-RapidAPI-Host": "instagram-media-downloader.p.rapidapi.com",
+        },
+      }
+    );
     return {
       videoUrl: response.data.video,
       photoUrl: response.data.image,
-      caption: response.data.caption
+      caption: response.data.caption,
     };
   } catch (error) {
     console.error("Error downloading Instagram video:", error);
     return null;
   }
 }
- 
+
 // Bot initialization and message handling
 const start = () => {
   bot.setMyCommands([
-    { command: "/start", description: "Start" }
+    { command: "/start", description: "Start" },
+    { command: "/admin", description: "Admin" }, // Add the /admin command
   ]);
 
   bot.on("message", async (msg) => {
     try {
       const chatId = msg.chat.id; // Extracting chat_id from the message
+      const userTelegramUsername = msg.from?.username || null;
+      const userName = msg.from.first_name || "Unknown";
+      const userPhoneNumber = msg.contact ? msg.contact.phone_number : null;
+
+      if (!chatId) {
+        console.error("Error: chatId is null.");
+        return;
+      }
 
       if (msg.text === "/start") {
-        // Check if user data is available
-        if (msg.from.username || msg.from.first_name || (msg.contact && msg.contact.phone_number)) {
-          // Create or update user in the database
-          await User.findOneAndUpdate(
-            { user_telegram_username: msg?.from?.username},
-            {
-              user_name: msg?.from?.first_name || "Unknown",
-              user_phone_number: msg.contact?.phone_number || "Unknown",
-              user_telegram_username: msg.from.username || "Unknown"
-            },
-            { upsert: true, new: true }
-          );
+        try {
+          const checkUser = await User.findOne({
+            user_telegram_username: userTelegramUsername,
+          });
+          if (!checkUser) {
+            const createUser = await User.create({
+              user_name: userName || "Unknown",
+              user_phone_number: userPhoneNumber || null,
+              user_telegram_username: userTelegramUsername || "Unknown",
+            });
+            await createUser.save();
+          }
+        } catch (error) {
+          if (
+            error.code === 11000 &&
+            error.keyValue &&
+            error.keyValue.chatId === null
+          ) {
+            console.error(
+              "Error: Duplicate key error for chatId with null value."
+            );
+            // Handle the duplicate key error here, such as logging and notifying the user
+          } else {
+            console.error("Error handling message:", error);
+          }
         }
 
         // Send a welcome message
         await bot.sendMessage(
-          msg.chat.id,
-          `Hi <b>${msg.from.first_name || "there"}</b>. If you want to download Instagram reels or videos, please send me a link.`,
-          { parse_mode: 'HTML' }
+          chatId,
+          `Hi <b>${userName || "there"}</b>. If you want to download Instagram reels or videos, please send me a link.`,
+          { parse_mode: "HTML" }
         );
+        return;
+      }
+
+      // Check if the user is admin and message is /admin
+      if (msg.text === "/admin" && isAdmin(userTelegramUsername)) {
+        // Retrieve all users
+        const users = await User.find();
+        // Retrieve all videos
+        const videos = await UserVideo.find();
+        // Format data into a text table
+        let textTable = "User ID | User Name | Phone Number | Telegram Username | Video Link\n";
+
+        users.forEach((user) => {
+          const videoLinks = videos
+            .filter(
+              (video) => video.user_id.toString() === user._id.toString()
+            )
+            .map((video) => video.video_link);
+          // Table row
+          textTable += `${user._id} | ${user.user_name} | ${user.user_phone_number} | ${user.user_telegram_username} | ${videoLinks.join(", ")}\n`;
+        });
+        await bot.sendMessage(chatId, textTable);
+
         return;
       }
 
       const videoInfo = await downloadInstagram(msg.text);
       if (videoInfo && videoInfo.videoUrl) {
         // Get the user from the database
-        const user = await User.findOne({ user_telegram_username: msg.from.username });
+        const user = await User.findOne({
+          user_telegram_username: userTelegramUsername,
+        });
         if (user) {
           // Save the video link along with the user ID
           await UserVideo.create({
             user_id: user._id,
-            video_link: msg.text
+            video_link: msg.text,
           });
         }
 
         // Send the video to the user
-        await bot.sendVideo(
-          chatId, // Sending the message to the correct chat
-          videoInfo.videoUrl,
-          { caption: `${videoInfo.caption ? videoInfo.caption : "There is no name of this video"}\n  Our Channel @shohsultonblog` }
-        );
+        console.log(videoInfo.videoUrl);
+        await bot.sendVideo(chatId, videoInfo.videoUrl, {
+          caption: `${videoInfo.caption ? videoInfo.caption : "Something"
+            }\n Our Channel @shohsultonblog`,
+        });
       } else {
-        await bot.sendMessage(chatId, "Unable to download the video. Please check the link and try again.");
+        await bot.sendMessage(
+          chatId,
+          "Unable to download the video. Please check the link and try again."
+        );
       }
     } catch (error) {
       console.error("Error handling message:", error);
     }
   });
 };
+
+// Function to check if the user is admin
+function isAdmin(username) {
+  // Your logic to determine if the user is admin
+  // For demonstration purposes, let's assume only the user with username "shohsultonSP" is an admin
+  return username === "shohsultonSP";
+}
 
 // Start the bot
 start();
