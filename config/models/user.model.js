d8913bc230588dd588dbe3d88dc8b9e7e14e0048
shohsultoncode <shohsulton.3039@gmail.com>
2024-06-01 14:58:35 +0500

    add: last fucking commit

--- config/models/user.model.js
+++ config/models/user.model.js
@@ -1,16 +1,9 @@
 const { Schema, model } = require("mongoose");
 
 const userModel = new Schema({
-  user_name: {
-    type: String,
-  },
-  user_phone_number: {
-    type: Number,
-  },
-  user_telegram_username: {
-    type: String,
-  }
-
+  user_name: String,
+  user_phone_number: String, // Change the type to String
+  user_telegram_username: String
 },
   {
     timestamps: true
